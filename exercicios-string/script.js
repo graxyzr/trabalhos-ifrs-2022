fafca03f52e3a0cbfa9a0434ea2d7cf7855218c7
Greice Pereira <118115939+graxyzr@users.noreply.github.com>
2024-01-23 18:53:06 -0300

    att

--- exercicios-string/script.js
+++ exercicios-string/script.js
@@ -1,11 +1,9 @@
 // Lista de Exercícios 4 (String) - Greice Pereira 1C1
 
 // 1 - Escreva um programa que codifica palavras da língua inglesa em Pig Latin.
-
 var p, a;
 
 p = prompt("Insira uma palavra:");
-
 a = p.substr(0,1);
 
 alert(p.substring(1) + a + "ay");
@@ -13,17 +11,15 @@ alert(p.substring(1) + a + "ay");
 
 
 // 2 - Escreva um programa que leia uma palavra ou frase e também leia uma letra. O programa deve mostrar quantas vezes a letra aparece na palavra ou frase.
-
 var l, p, r, l2 = " ";
 
 p = prompt("Insira sua palavra/frase:");
-
 l = prompt("Insira sua letra:");
 
 for (c = 0 ; c < p.length ; c++) {
-    l2 = p.charAt(c)
+    l2 = p.charAt(c);
     if (l == l2) {
-        r = l2.length
+        r = l2.length;
         console.log(l2);
     }
 }
@@ -31,7 +27,6 @@ for (c = 0 ; c < p.length ; c++) {
 
 
 // 3 - Escreva um programa que leia uma palavra sem acentos e cedilha e mostre essa palavra em letras minúsculas, com a primeira letra em maiúsculo.
-
 var p, a, c;
 
 p = prompt("Insira uma palavra:");
@@ -82,13 +77,10 @@ for (c = 0 ; c < p.length ; c++) {
 
 
 // 4 - Escreva um programa que leia o código DDD e um número de telefone e monte uma String no formato pedido, em que D representa um dígito do DDD e N um dígito do número de telefone.
-
 var n, d, e = " ";
 
 n = prompt("Insira o número de telefone:");
-
 d = n.substring(0,2);
-
 n2 = n.substring(6,11);
 
 alert("(" + d + ")" + e +  n.substring(2,6) + "-" + n2);
@@ -96,13 +88,11 @@ alert("(" + d + ")" + e +  n.substring(2,6) + "-" + n2);
 
 
 // 5 - Escreva um programa que leia um String contendo um número de telefone no formato pedido.
-
 var dn, ddd, n;
 
 dn = prompt("Insira o número de telefone:");
 
 if ( dn.charAt(0) == "(" && dn.charAt(3) == ")" || dn.charAt(4) == " " && dn.charAt(9) == "-") {
-    
     ddd = dn.substring(1,3);
     alert("DDD: " + ddd);
     
@@ -117,7 +107,6 @@ else {
 
 
 // 6 - Escreva um programa que é capaz de gerar o plural de algumas palavras em inglês. O programa deve ler uma palavra, convertê-la para letras minúsculas, e mostrar seu plural na tela.
-
 var p;
 
 p = prompt("Insira uma palavra em inglês:").toLowerCase();
@@ -125,9 +114,11 @@ p = prompt("Insira uma palavra em inglês:").toLowerCase();
 if (p.endsWith("o") || p.endsWith("ch") || p.endsWith("sh") || p.endsWith("ss") || p.endsWith("x") || p.endsWith("z")) {
     alert(p + "es");
 }
+
 else if (p.endsWith("y")) {
     alert(p.replace("y","ies"));
 }
+
 else {
     alert(p + "s");
 }
@@ -135,7 +126,6 @@ else {
 
 
 // 7 - Escreva um programa que lê uma data no primeiro formato (como String) e mostre essa data no formato pedido.
-
 var d;
 
 d = prompt("Insira uma data:");
@@ -143,36 +133,47 @@ d = prompt("Insira uma data:");
 if (d.charAt(3) == "0" && d.charAt(4) == "1") {
     alert(d.substring(0,2) + " de janeiro de " + d.substring(6));
 }
+
 if (d.charAt(3) == "0" && d.charAt(4) == "2") {
     alert(d.substring(0,2) + " de fevereiro de " + d.substring(6));
 }
+
 if (d.charAt(3) == "0" && d.charAt(4) == "3") {
     alert(d.substring(0,2) + " de março de " + d.substring(6));
 }
+
 if (d.charAt(3) == "0" && d.charAt(4) == "4") {
     alert(d.substring(0,2) + " de abril de " + d.substring(6));
 }
+
 if (d.charAt(3) == "0" && d.charAt(4) == "5") {
     alert(d.substring(0,2) + " de maio de " + d.substring(6));
 }
+
 if (d.charAt(3) == "0" && d.charAt(4) == "6") {
     alert(d.substring(0,2) + " de junho de " + d.substring(6));
 }
+
 if (d.charAt(3) == "0" && d.charAt(4) == "7") {
     alert(d.substring(0,2) + " de julho de " + d.substring(6));
 }
+
 if (d.charAt(3) == "0" && d.charAt(4) == "8") {
     alert(d.substring(0,2) + " de agosto de " + d.substring(6));
 }
+
 if (d.charAt(3) == "0" && d.charAt(4) == "9") {
     alert(d.substring(0,2) + " de setembro de " + d.substring(6));
 }
+
 if (d.charAt(3) == "1" && d.charAt(4) == "0") {
     alert(d.substring(0,2) + " de outubro de " + d.substring(6));
 }
+
 if (d.charAt(3) == "1" && d.charAt(4) == "1") {
     alert(d.substring(0,2) + " de novembro de " + d.substring(6));
 }
+
 if (d.charAt(3) == "1" && d.charAt(4) == "2") {
     alert(d.substring(0,2) + " de dezembro de " + d.substring(6));
 }
@@ -180,64 +181,52 @@ if (d.charAt(3) == "1" && d.charAt(4) == "2") {
 
 
 // 8 - Escreva um programa que leia uma data no formato DD/MM/AAAA como String e diga se a data é valida ou não.
-
 var dc, d, m, a;
 
 dc = prompt("Insira uma data:");
 
 if (dc.length == 10 && dc.charAt(2) == "/" || dc.charAt(2) == "-" || dc.charAt(2) == "." && dc.charAt(5) == "/" || dc.charAt(5) == "-" || dc.charAt(5) == ".") {
-
     alert("O formato da data é válido!");
-
 }
-    else {
 
-        alert("O formato da data é INVÁLIDO!");
-
-    }
-
+else {
+    alert("O formato da data é INVÁLIDO!");
+}
         d = dc.substring(0,2);
         m = dc.substring(3,5);
         a = dc.substring(6,9);
 
-        if (m == 01 || m == 02 || m == 03 || m == 04 || m == 05 || m == 06 || m == 07 || m == 08 || m == 09 || m == 10 || m == 11 || m == 12) {
+if (m == 01 || m == 02 || m == 03 || m == 04 || m == 05 || m == 06 || m == 07 || m == 08 || m == 09 || m == 10 || m == 11 || m == 12) {
+    alert("O mês da data é válido!");
+}
 
-            alert("O mês da data é válido!");
-
-        }
-            else {
-
-                alert("O mês da data é INVÁLIDO!");
-
-            }
+else {
+    alert("O mês da data é INVÁLIDO!");
+}
                 
-        if (d >= 01 && d <= 31 || d <= 30 || d <= 28) {
-            
-            alert("O dia da data é válido!");
+if (d >= 01 && d <= 31 || d <= 30 || d <= 28) {            
+    alert("O dia da data é válido!");
 
-        }
-            else {
+}
 
-                alert("O dia da data é INVÁLIDO!");
-                
-            }
+else {
+    alert("O dia da data é INVÁLIDO!");                
+}
 
 
 
 // 9 - Escreva um programa que verifique se uma string é ou não um palíndromo.
-
 var p, c, r = "";
 
 p = prompt("Digite uma palavra para verificar se é um palíndromo:").replaceAll(" ","");
 
 for( c = p.length - 1 ; c >= 0 ; c-- ) {
-
     r = r + p.charAt(c);
-
 }
     if ( r == p ) {
         alert("A String é um palíndromo!");
     }
+
     else {
         alert("A String NÃO É um palíndromo!");
     }
@@ -249,14 +238,11 @@ for( c = p.length - 1 ; c >= 0 ; c-- ) {
 var n, p1;
 
 n = prompt("Insira um nome:");
-
 p1 = n.charAt(0);
 
 for ( c = 0 ; c < n.length ; c++ ) {
     if (n.charAt(c) == " ") {
         if (n.substring(c + 1, c + 3) != "e " && n.substring(c + 1, c + 4) != "do " && n.substring(c + 1, c + 4) != "da " && n.substring(c + 1, c + 4) != "de " && n.substring(c + 1, c + 4) != "di " && n.substring(c + 1, c + 4) != "du " && n.substring(c + 1, c + 5) != "dos " && n.substring(c + 1, c + 5) != "das ") {
-
-        
         p1 += n.charAt(c + 1);
     }
 }
