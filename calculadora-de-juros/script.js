3b04a47fd179e9ad1f715268886f0a59a43a53b4
Greice Pereira <118115939+graxyzr@users.noreply.github.com>
2024-07-15 18:17:54 -0300

    att

--- calculadora-de-juros/script.js
+++ calculadora-de-juros/script.js
@@ -1,23 +1,13 @@
 // Trabalho de Programação c/ Matemática - Greice Pereira 1C1
-
 function calculaJuros(capital, taxaJuros, tempoAplicado) {
-
-    var taxaJuros = taxaJuros / 100;
-    var montante = capital * Math.pow((1 + taxaJuros), tempoAplicado);
-
-
-    var total = montante.toFixed(2);
-    var totalm = total / tempoAplicado;
-    totalm = totalm.toFixed(2);
-
-    return "Total: R$" + total + "\nMensalmente: R$" + totalm;
-
-
+	var taxaJuros = taxaJuros / 100;
+	var montante = capital * Math.pow((1 + taxaJuros), tempoAplicado);
+	var total = montante.toFixed(2);
+	var totalm = total / tempoAplicado;
+	totalm = totalm.toFixed(2);
+	return "Total: R$" + total + "\nMensalmente: R$" + totalm;
 }
-
 var c = Number(prompt("Insira o valor da capital:"));
 var tJ = Number(prompt("Insira o valor da taxa:"));
 var t = Number(prompt("Insira o tempo de aplicação:"));
-
-
 console.log(calculaJuros(c, tJ, t));
