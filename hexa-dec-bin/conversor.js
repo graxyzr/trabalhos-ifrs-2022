3d6bb2c917d628bb0c9edd73c33b90d1d55af249
Greice Pereira <118115939+graxyzr@users.noreply.github.com>
2024-07-15 18:11:21 -0300

    att

--- hexa-dec-bin/conversor.js
+++ hexa-dec-bin/conversor.js
@@ -1,8 +1,4 @@
 var h = prompt("Informe um número hexadecimal:");
-
 var decimal = parseInt(h, 16);
 var binario = decimal.toString(2);
-
-alert("Número Hexadecimal: " + h +
-    "\nNúmero Decimal: " + decimal +
-    "\nNúmero Binário: " + binario);
+alert("Número Hexadecimal: " + h + "\nNúmero Decimal: " + decimal + "\nNúmero Binário: " + binario);
